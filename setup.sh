4f7fc2230d18c77866639acd495bb657bc1f669e
kiosuper <32238626+kiosuper@users.noreply.github.com>
2019-08-14 09:31:52 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+RED='\033[0;31m'
+PURP='\033[0;35m'
+
+if [[ $EUID -ne 0 ]]; then
+	printf "${RED}This script must be run as root.\n"
+	printf "${PURP}Usage: ${RED}./setup.sh [USERNAME]\n"
+	exit
+else
+	MY_NAME=$(echo $HOME | cut -c 7-50)
+	apt update 
+	apt install vim openssh-server nmap -y
+	service ssh start
+	printf "${PURP}[+] ${RED}vim, ssh, nmap Done.\n"
+	apt-get update
+	apt-get install python2.7 python-pip python-dev git libssl-dev libffi-dev build-essential -y
+	pip install --upgrade pip
+	pip install --upgrade pwntools
+	printf "${PURP}[+] ${RED}pwntool Done.\n"
+	su -c "mkdir tools" $MY_NAME
+	cd tools
+	su -c "git clone https://github.com/apogiatzis/gdb-peda-pwndbg-gef.git" $MY_NAME
+	cd gdb-peda-pwndbg-gef
+	./install.sh
+	printf "${PURP}[+] ${RED}GDB Done.\n"
+	cd $HOME/tools
+	rm -rf *
+	su -c "git clone https://github.com/kiosuper/attach.git" $MY_NAME
+	echo "source $HOME/tools/attach/at.py" >> $HOME/.gdbinit
+	echo "define m" >> $HOME/.gdbinit
+	echo "disass main" >> $HOME/.gdbinit
+	echo "end" >> $HOME/.gdbinit
+	echo "define full" >> $HOME/.gdbinit
+	echo "set print elements 1000" >> $HOME/.gdbinit
+	echo "end" >> $HOME/.gdbinit
+	echo "define vm" >> $HOME/.gdbinit
+	echo "vmmap" >> $HOME/.gdbinit
+	echo "end" >> $HOME/.gdbinit
+	printf "${PURP}[+] ${RED}gdbinit Done.\n"
+	chown root:root $HOME/tools/attach/aslr
+	cp -v $HOME/tools/attach/aslr /usr/bin
+	chmod +x /usr/bin/aslr
+	printf "${PURP}[+] ${RED}ASLR Done.\n"
+	pip install capstone
+	pip install keystone-engine
+	pip install "filebytes<0.9.20" ropper
+	printf "${PURP}[+] ${RED}ropper Done.\n"
+	#apt-get install qemu qemu-user-static kpartx gdb-multiarch -y
+fi
