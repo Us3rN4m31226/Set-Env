035466ba0e4b25f16e876b92b6f052a6bc30ff4c
kiosuper <32238626+kiosuper@users.noreply.github.com>
2019-09-19 10:12:47 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 RED='\033[0;31m'
 PURP='\033[0;35m'
+WHITE="\[\033[0;37m\]"
 
 if [[ $EUID -ne 0 ]]; then
-	printf "${RED}This script must be run as root.\n"
-	printf "${PURP}Usage: ${RED}./setup.sh [USERNAME]\n"
+	printf "${RED}This script must be run as root.\n${WHITE}"
+	printf "${PURP}Usage: ${RED}./setup.sh [USERNAME]\n${WHITE}"
 	exit
 else
 	MY_NAME=$(echo $HOME | cut -c 7-50)
 	apt update 
 	apt install vim openssh-server nmap -y
 	service ssh start
-	printf "${PURP}[+] ${RED}vim, ssh, nmap Done.\n"
+	printf "${PURP}[+] ${RED}vim, ssh, nmap Done.\n${WHITE}"
 	apt-get update
 	apt-get install python2.7 python-pip python-dev git libssl-dev libffi-dev build-essential -y
 	pip install --upgrade pip
 	pip install --upgrade pwntools
-	printf "${PURP}[+] ${RED}pwntool Done.\n"
+	printf "${PURP}[+] ${RED}pwntool Done.\n${WHITE}"
 	su -c "mkdir tools" $MY_NAME
 	cd tools
 	su -c "git clone https://github.com/apogiatzis/gdb-peda-pwndbg-gef.git" $MY_NAME
 	cd gdb-peda-pwndbg-gef
 	./install.sh
-	printf "${PURP}[+] ${RED}GDB Done.\n"
+	printf "${PURP}[+] ${RED}GDB Done.\n${WHITE}"
 	cd $HOME/tools
 	rm -rf *
 	su -c "git clone https://github.com/kiosuper/attach.git" $MY_NAME
+	echo >> $HOME/.gdbinit
 	echo "source $HOME/tools/attach/at.py" >> $HOME/.gdbinit
 	echo "define m" >> $HOME/.gdbinit
 	echo "disass main" >> $HOME/.gdbinit
@@ -36,14 +38,14 @@ else
 	echo "define vm" >> $HOME/.gdbinit
 	echo "vmmap" >> $HOME/.gdbinit
 	echo "end" >> $HOME/.gdbinit
-	printf "${PURP}[+] ${RED}gdbinit Done.\n"
+	printf "${PURP}[+] ${RED}gdbinit Done.\n${WHITE}"
 	chown root:root $HOME/tools/attach/aslr
 	cp -v $HOME/tools/attach/aslr /usr/bin
 	chmod +x /usr/bin/aslr
-	printf "${PURP}[+] ${RED}ASLR Done.\n"
+	printf "${PURP}[+] ${RED}ASLR Done.\n${WHITE}"
 	pip install capstone
 	pip install keystone-engine
 	pip install "filebytes<0.9.20" ropper
-	printf "${PURP}[+] ${RED}ropper Done.\n"
+	printf "${PURP}[+] ${RED}ropper Done.\n${WHITE}"
 	#apt-get install qemu qemu-user-static kpartx gdb-multiarch -y
 fi
